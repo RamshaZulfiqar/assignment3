f399656849dd24398cbfdfaf8729a529ce9f5327
RamshaZulfiqar <73095632+RamshaZulfiqar@users.noreply.github.com>
2020-11-06 11:20:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
+
        
           ramsha zulfiqar 2017383 ffff
   
-      </header>
     </div>
   );
 }
